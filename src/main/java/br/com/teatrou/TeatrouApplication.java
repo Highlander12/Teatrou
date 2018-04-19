28520e4756ec219fccfa08fd8f92cae4cde003a4
Highlander Martins Dantas <haylander60@gmail.com>
2018-04-18 22:06:59 -0300

    Criação do projeto.

---
+++ src/main/java/br/com/teatrou/TeatrouApplication.java
@@ -0,0 +1,12 @@
+package br.com.teatrou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatrouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatrouApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/teatrou/TeatrouApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.teatrou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeatrouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
